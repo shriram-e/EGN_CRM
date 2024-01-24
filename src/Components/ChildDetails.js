01466bfec2ff1f9c222cb8f63646384943da4afe
Shri Ram Elangovan <105792234+shriram-e@users.noreply.github.com>
2024-01-24 15:10:39 +0000

    "/c/Edwin/EGN/CRM/egn_crm/.git/COMMIT_EDITMSG" [unix] 13L, 310B

--- src/Components/ChildDetails.js
+++ src/Components/ChildDetails.js
@@ -4,11 +4,13 @@ import FormTextArea from './FormTextArea';
 import FormRadio from './FormRadio';
 
 const ChildDetails = (props) => {
+    const Childname = 'Child_Full_Name_' + props.Nos
+    const ChildDOB = 'Child_DOB_' + props.Nos
     return (
         <div>
             <p>Child Details - {props.Nos}</p>
-            <FormInput label='Full Name' Placeholder='Child Name' type='text' name='Child_Full_Name' />
-            <FormInput label='Date of birth' Placeholder='DD/MM/YYYY' type='text' name='Child_DOB' />
+            <FormInput label='Full Name' Placeholder='Child Name' type='text' name={Childname} />
+            <FormInput label='Date of birth' Placeholder='DD/MM/YYYY' type='text' name={ChildDOB} />
         </div>
     )
 }
